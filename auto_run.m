2325087df13f05157620f32829bd12a95af1f2c9
David Huberdeau <david.huberdeau@gmail.com>
2017-08-28 15:45:53 -0400

    added auto_run script
    One script for running all Matlab analyses of adaptation_proceduralization project. Other analyses in R.

---
+++ auto_run.m
@@ -0,0 +1,12 @@
+% Run all analyses for the adaptation_proceduralization project. Reference:
+%
+% Huberdeau, D.; Haith, A.; Krakauer, J. In preparation. 2017
+%
+% Date: 08/28/2017
+
+analyze_experiment1_v8;
+analyze_experiment2_v8;
+
+% The last step is to run the following scripts in R:
+% rate_interaction_test_E2
+% rate_interaction_test_E2_B
